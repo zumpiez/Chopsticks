17485948abf46871d2fde8dcf03f4d04eebf3e0a
Jeff Hutchins <zumpiez@gmail.com>
2012-04-24 00:20:14 -0700

    Basic nunit test

---
+++ Chopsticks/Chopsticks/StupidTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics;
+
+namespace Chopsticks
+{
+    [TestFixture]
+    class StupidTest
+    {
+        [Test]
+        public void TestStupidThing()
+        {
+            Assert.That(true == true);
+        }
+
+    }
+}
