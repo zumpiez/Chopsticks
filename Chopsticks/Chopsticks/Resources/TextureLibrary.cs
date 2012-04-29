ab4d574d05c326a0c0e258606387be65360a6f15
Jeff Hutchins <zumpiez@gmail.com>
2012-04-28 20:02:56 -0700

    Update TextureLibrary to cache textures in an atlas

--- Chopsticks/Chopsticks/Resources/TextureLibrary.cs
+++ Chopsticks/Chopsticks/Resources/TextureLibrary.cs
@@ -10,22 +10,49 @@ namespace Chopsticks.Resources
     class TextureLibrary
     {
         private GraphicsDevice device;
+        private Dictionary<String, Texture2D> atlas;
 
         public TextureLibrary(GraphicsDevice device, string path, string defaultExtension = ".png")
         {
             this.device = device;
             this.Path = path;
             this.FileExtension = defaultExtension;
+            atlas = new Dictionary<string, Texture2D>();
         }
 
+        /// <summary>
+        /// Fetches a texture from the atlas. If the texture is not present in the atlas, it is loaded and added.
+        /// </summary>
+        /// <param name="name">The name of the texture</param>
+        /// <returns>The texture instance</returns>
         public Texture2D GetTexture(string name)
         {
-            using (var file = File.OpenRead(Path + name + FileExtension))
+            //lookup texture from atlas
+            if (atlas.ContainsKey(name))
             {
-                return Texture2D.FromStream(device, file);
+                return atlas[name];
+            }
+            else
+            {
+                return InitializeTexture(name);
             }
         }
 
+        /// <summary>
+        /// Initialize texture in atlas
+        /// </summary>
+        /// <param name="name">Name of texture.</param>
+        /// <returns>Texture2D that has been inserted into the atlas</returns>
+        private Texture2D InitializeTexture(string name)
+        {
+            using (var file = File.OpenRead(Path + name + FileExtension))
+            {
+                var texture = Texture2D.FromStream(device, file);
+                atlas[name] = texture;
+                return texture;
+            }
+        }
+        
         public string Path { get; set; }
 
         public string FileExtension { get; set; }
