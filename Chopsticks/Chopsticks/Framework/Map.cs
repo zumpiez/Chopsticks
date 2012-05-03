30b8563671ba9723854416563fcb628fceb30a28
Jeff Hutchins <zumpiez@gmail.com>
2012-05-02 22:00:29 -0700

    Add Map and Transformation classes to new Framework namespace and move TextureAtlas to framework namespace

---
+++ Chopsticks/Chopsticks/Framework/Map.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chopsticks
+{
+    public class Map
+    {
+        public Dictionary<String, Transformation> Scene { get; private set; }
+    }
+}
---
+++ Chopsticks/Chopsticks/Framework/Transformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Chopsticks
+{
+    public struct Transformation
+    {
+        public Vector3 Translation { get; set; }
+        public float Rotation { get; set; }
+        public Vector2 scale { get; set; }
+    }
+}
